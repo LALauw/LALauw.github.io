175e802e03e087e05d22102daf717ff6296bd714
Leslie Lauw <32265116+LALauw@users.noreply.github.com>
2020-05-13 15:45:47 +0200

    redone grid for projects. More flexible.

--- src/components/section/Projects.js
+++ src/components/section/Projects.js
@@ -19,9 +19,10 @@ function Projects() {
         <h2 className="heading">{t("project.titel")}</h2>
         <p>{t("project.hint")}</p>
         <div className="content">
-          <div className="item featured text-center">
+          <div className="item featured text-center ">
             <div className="rowoverrideproject">
-              <div className="coloverride">
+              <div className="coloverrideproject ">
+                <h3 className="heading-project">Outdoor paradise</h3>
                 <a
                   onClick={() => setState("outdoor")}
                   className="figureproject hoveranim"
@@ -33,7 +34,8 @@ function Projects() {
                   />
                 </a>
               </div>
-              <div className="coloverride">
+              <div className="coloverrideproject">
+                <h3 className="heading-project">Keukenhof</h3>
                 <a
                   onClick={() => setState("keukenhof")}
                   className="figureproject hoveranim"
@@ -45,7 +47,8 @@ function Projects() {
                   />
                 </a>
               </div>
-              <div className="coloverride">
+              <div className="coloverrideproject">
+                <h3 className="heading-project">MazaGameJLR</h3>
                 <a
                   onClick={() => setState("maze")}
                   className="figureproject hoveranim"
@@ -57,7 +60,8 @@ function Projects() {
                   />
                 </a>
               </div>
-              <div className="coloverride">
+              <div className="coloverrideproject">
+                <h3 className="heading-project">Personal Portfolio</h3>
                 <a
                   onClick={() => setState("portfolio")}
                   className="figureproject hoveranim"
@@ -69,8 +73,9 @@ function Projects() {
                   />
                 </a>
               </div>
-              {/*
-              <div className="coloverride">
+
+              <div className="coloverrideproject">
+                <h3 className="heading-project">RoboShaq Shooter</h3>
                 <a
                   onClick={() => setState("knights")}
                   className="figureproject hoveranim"
@@ -82,7 +87,6 @@ function Projects() {
                   />
                 </a>
               </div>
-*/}
             </div>
           </div>
           <ProjectsInfoSwitch soort={whichComponentToShow} />
